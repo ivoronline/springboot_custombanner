0a7aa5ba377006b4fbaa29dbb8f83bcfbfae0e17
igoreta <ivoronline@gmail.com>
2021-02-26 11:38:50 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_custombanner/SpringbootCustombannerApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_custombanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustombannerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootCustombannerApplication.class, args);
+  }
+
+}
